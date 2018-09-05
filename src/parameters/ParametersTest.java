f3aa6853dc936a8442279693da7aafa4e0a4ea73
josdoaitran <josdoaitran@gmail.com>
2018-09-05 23:35:15 +0700

    param

---
+++ src/parameters/ParametersTest.java
@@ -0,0 +1,27 @@
+package parameters;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ParametersTest {
+    private static WebDriver driver;
+
+    @Test
+    @Parameters({ "sUsername", "sPassword" })
+    public void test(String sUsername, String sPassword) {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.store.demoqa.com");
+        driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+        driver.findElement(By.id("log")).sendKeys(sUsername);
+        driver.findElement(By.id("pwd")).sendKeys(sPassword);
+        driver.findElement(By.id("login")).click();
+        driver.findElement(By.xpath(".//*[@id='account_logout']/a")).click();
+        driver.quit();
+    }
+}
